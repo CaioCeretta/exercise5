d71b011091b18e03ff46934382595cf200a5803b
Caio Ceretta <caioceretta@gmail.com>
2020-09-03 08:48:07 -0300

    Correções

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,6 @@
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-import routes from './routes';
-import AppError from './errors/AppError';
 
-import './database';
-
-const app = express();
-app.use(express.json());
-app.use(routes);
-
-app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return response.status(err.statusCode).json({
-      status: 'error',
-      message: err.message,
-    });
-  }
-
-  console.error(err);
-
-  return response.status(500).json({
-    status: 'error',
-    message: 'Internanal server error',
-  });
-
-});
+import app from './app';
 
 app.listen(3333, () => {
-  console.log('🚀 Server Started on Port 3333');
+  console.log('🚀 Server started on port 3333!');
 });
